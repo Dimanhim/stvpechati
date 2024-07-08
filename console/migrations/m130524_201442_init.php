8e6fa2f9a45d4d4700c4cb467f2a7cbe119795e4
Dimanhim <dimanhim@list.ru>
2024-07-08 23:21:04 +0300

    prepare project for deployment

--- console/migrations/m130524_201442_init.php
+++ console/migrations/m130524_201442_init.php
@@ -14,16 +14,35 @@ class m130524_201442_init extends Migration
 
         $this->createTable('{{%user}}', [
             'id' => $this->primaryKey(),
+
             'username' => $this->string()->notNull()->unique(),
+            'name' => $this->string()->notNull()->unique(),
             'auth_key' => $this->string(32)->notNull(),
+            'password' => $this->string(),
             'password_hash' => $this->string()->notNull(),
             'password_reset_token' => $this->string()->unique(),
             'email' => $this->string()->notNull()->unique(),
 
             'status' => $this->smallInteger()->notNull()->defaultValue(10),
+
+            'is_active' => $this->smallInteger()->defaultValue(1),
+            'deleted' => $this->smallInteger(),
+            'position' => $this->integer(),
             'created_at' => $this->integer()->notNull(),
             'updated_at' => $this->integer()->notNull(),
         ], $tableOptions);
+
+        $this->insert('{{%user}}', [
+            'username' => 'admin',
+            'name' => 'Администратор',
+            'auth_key' => Yii::$app->security->generateRandomString(),
+            'password' => '123456',
+            'password_hash' => '$2y$13$1fnAOhEBVzCcOfl3n2QXaO4ai1dQyK2eOUL4CcOc9c5sdkEYe9X.O',
+            'email' => 'dimanhim@list.ru',
+            'status' => '10',
+            'created_at' => time(),
+            'updated_at' => time()
+        ]);
     }
 
     public function down()
