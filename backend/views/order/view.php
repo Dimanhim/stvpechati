c667ef440c9d35578fe00a576281049148608bf2
Dimanhim <dimanhim@list.ru>
2024-07-28 00:39:46 +0300

    order view

--- backend/views/order/view.php
+++ backend/views/order/view.php
@@ -7,7 +7,7 @@ use yii\widgets\DetailView;
 /** @var common\models\Order $model */
 
 $this->title = $model->name;
-$this->params['breadcrumbs'][] = ['label' => 'Orders', 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => $model->modelName, 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
 ?>
