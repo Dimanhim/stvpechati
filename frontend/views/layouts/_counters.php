a34bfe055874c4007482d24a1cdd1fff492767b1
Dimanhim <dimanhim@list.ru>
2024-08-02 23:13:17 +0300

    яндекс метрика 2

--- frontend/views/layouts/_counters.php
+++ frontend/views/layouts/_counters.php
@@ -7,7 +7,7 @@
         k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
     (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-    ym(97983753, "init", {
+    ym(<?= $_ENV['YANDEX_METRIKA_COUNTER'] ?>, "init", {
         clickmap:true,
         trackLinks:true,
         accurateTrackBounce:true,
